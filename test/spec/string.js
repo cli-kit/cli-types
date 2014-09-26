240784e656cd1539c24c96c5fb3e4f90e0f6a60b
cyberfunk <hello@cyberfunk.org>
2014-09-27 00:02:15 +0800

    Initial files.

---
+++ test/spec/string.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var sys = require('../..');
+
+describe('cli-types:', function() {
+  it('should validate string type', function(done) {
+    done();
+  });
+})
