010b945a9de55d97d7c6b1d09dbeb643a7bad67e
cyberfunk <hello@cyberfunk.org>
2014-09-27 11:38:10 +0800

    Add array type tests.

--- test/spec/string.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect;
-var sys = require('../..');
-
-describe('cli-types:', function() {
-  it('should validate string type', function(done) {
-    done();
-  });
-})
---
+++ test/spec/types/array.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect;
+var types = require('../../..');
+
+var define = require('cli-define')
+  , Option = define.Option;
+
+describe('cli-types:', function() {
+  it('should be an array (single value)', function(done) {
+    var value = 'value';
+    var opt = new Option(
+      '-a, --array <a>', 'an array argument', types.array);
+    var res = types.array(value, opt);
+    expect(res).to.eql([value]);
+    done();
+  });
+  it('should be an array (multiple value)', function(done) {
+    var value = 'value';
+    var opt = new Option(
+      '-a, --array <a>', 'an array argument', types.array);
+    var res = types.array([value, value, value], opt);
+    expect(res).to.eql([value, value, value]);
+    done();
+  });
+})
