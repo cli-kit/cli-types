e9b30fc9c4eb6d5573d264443a817ce765301937
cyberfunk <hello@cyberfunk.org>
2014-09-27 12:15:18 +0800

    Add json type tests.

---
+++ test/spec/types/json.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect
+  , types = require('../../..')
+  , ArgumentTypeError = types.ArgumentTypeError
+  , define = require('cli-define')
+  , Option = define.Option;
+
+describe('cli-types:', function() {
+  it('should be a parsed object (json)', function(done) {
+    var value =
+      {string: 'value', number: 128, boolean: true, obj: {}, arr: [1,2,3]};
+    var stringified = JSON.stringify(value);
+    var opt = new Option('-j, --json <j>', 'a json argument')
+    var res = types.json(stringified, opt);
+    expect(res).to.eql(value);
+    done();
+  });
+  it('should error on malformed json', function(done) {
+    var stringified = '"escaped \" quote and malformed escape \<"';
+    var opt = new Option('-j, --json <j>', 'a json argument')
+    function func() {
+      types.json(stringified, opt);
+    }
+    expect(func).to.throw(ArgumentTypeError);
+    done();
+  });
+})
