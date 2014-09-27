c74469327284ad1f4b0317858b8b4e53584bc9f3
cyberfunk <hello@cyberfunk.org>
2014-09-27 14:17:48 +0800

    Improve list array support.
    Update list tests.

--- test/spec/types/list.js
+++ test/spec/types/list.js
@@ -5,6 +5,7 @@ var expect = require('chai').expect
   , Option = define.Option;
 
 describe('cli-types:', function() {
+
   it('should be an array list (single value)', function(done) {
     var value = 'value';
     var opt = new Option(
@@ -14,6 +15,7 @@ describe('cli-types:', function() {
     expect(res).to.eql([value]);
     done();
   });
+
   it('should be an array list (multiple value)', function(done) {
     var value = 'apple , orange,pear, mango';
     var opt = new Option(
@@ -21,6 +23,21 @@ describe('cli-types:', function() {
     var converter = types.list(/\s*,\s*/);
     var res = converter(value, opt);
     expect(res).to.eql(['apple', 'orange', 'pear', 'mango']);
+    done();
+  });
+
+  it('should be a multi-dimensional array list (repeatable)', function(done) {
+    var one = 'apple , orange,pear, mango';
+    var alt = 'banana, watermelon';
+    var value = [one, alt];
+    var expected = [
+      ['apple', 'orange', 'pear', 'mango'], ['banana', 'watermelon']
+    ];
+    var opt = new Option(
+      '-l, --list <list>', 'a comma-delimited list argument');
+    var converter = types.list(/\s*,\s*/);
+    var res = converter(value, opt);
+    expect(res).to.eql(expected);
     done();
   });
 });
