8fcc92d53cb956a9babf1c0f52cdb856f7335e1b
cyberfunk <hello@cyberfunk.org>
2014-09-27 15:23:14 +0800

    Assert on object groups.

--- test/util/scope.js
+++ test/util/scope.js
@@ -1,9 +1,14 @@
 var err = require('cli-error');
 var errors = require('../../lib/errors');
+var _configure = {stash: {}};
 
 var Scope = function() {
   err.load(errors);
   this.errors = err.errors;
+}
+
+Scope.prototype.configure = function() {
+  return _configure;
 }
 
 module.exports = new Scope();
