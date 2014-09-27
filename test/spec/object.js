a696341de71cd41970ee96ba357f02ff4edb3f52
cyberfunk <hello@cyberfunk.org>
2014-09-27 15:24:53 +0800

    Assert on object array groupings.

--- test/spec/object.js
+++ test/spec/object.js
@@ -40,22 +40,36 @@ describe('cli-types:', function() {
     done();
   });
 
-  //it('should group arguments into object (nested array)', function(done) {
-    //var cli = require('../../..')(pkg);
-    //var scheme = 'http';
-    //var node = 'nodejs.org';
-    //var npm = 'npmjs.org';
-    //var port = '80';
-    //var args = ['-s', scheme, '-h', node, '--host=' + npm, '--port=' + port];
-    //cli
-      //.option('-s, --scheme <scheme>',
-        //'transport scheme', types.object('server'))
-      //.option('-h, --host <host...>',
-        //'server hostname', types.object('server'))
-      //.option('-p, --port <n>',
-        //'server port', types.object('server'))
-    //cli.parse(args);
-    //expect(cli.server).to.eql({scheme: scheme, host: [node, npm], port: port });
-    //done();
-  //});
+  it('should group arguments into object (nested array)', function(done) {
+    var expected = {
+      scheme: 'http',
+      host: ['nodejs.org', 'npmjs.org'],
+      port: '80'
+    }
+    var scheme =
+        new Option('-s, --scheme <scheme>',
+        'transport scheme', types.object('server'));
+    var host =
+      new Option('-h, --host <host>',
+        'server hostname', types.object('server'));
+    var port =
+      new Option('-p, --port <n>',
+        'server port', types.object('server'))
+
+    var res = scheme.converter().call(scope, expected.scheme, scheme);
+    expect(res).to.be.an('object');
+    expect(res.scheme).to.eql(expected.scheme);
+
+    res = host.converter().call(scope, expected.host, host);
+    expect(res).to.be.an('object');
+    expect(res.host).to.eql(expected.host);
+
+    res = port.converter().call(scope, expected.port, port);
+    expect(res).to.be.an('object');
+    expect(res.port).to.eql(expected.port);
+
+    expect(res).to.eql(scope.configure().stash.server)
+      .to.eql(expected);
+    done();
+  });
 })
