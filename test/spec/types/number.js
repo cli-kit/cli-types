396f4666cad6033dea651ca48ab4f20976c4581e
cyberfunk <hello@cyberfunk.org>
2014-09-27 14:13:58 +0800

    Update number tests.

--- test/spec/types/number.js
+++ test/spec/types/number.js
@@ -6,6 +6,9 @@ var expect = require('chai').expect
 
 describe('cli-types:', function() {
   var pi = 3.14159265359;
+  var golden = 1.61803398875;
+  var integer = 128;
+
   it('should coerce single value to number', function(done) {
     var value = '' + pi;
     var opt = new Option(
@@ -14,6 +17,7 @@ describe('cli-types:', function() {
     expect(res).to.eql(pi);
     done();
   });
+
   it('should error on invalid number', function(done) {
     var value = 'xyz';
     var opt = new Option(
@@ -22,6 +26,27 @@ describe('cli-types:', function() {
       types.number(value, opt);
     }
     expect(func).to.throw(ArgumentTypeError);
+    done();
+  });
+
+  it('should error on invalid number in array', function(done) {
+    var value = ['' + pi, '' + golden, 'xyz'];
+    var opt = new Option(
+      '-n, --number <number>', 'a number argument');
+    function func() {
+      types.number(value, opt);
+    }
+    expect(func).to.throw(ArgumentTypeError);
+    done();
+  });
+
+  it('should coerce multiple values to array of numbers', function(done) {
+    var value = ['' + pi, '' + golden, '' + integer];
+    var expected = [pi, golden, integer];
+    var opt = new Option(
+      '-n, --number <number>', 'a number argument');
+    var res = types.number(value, opt);
+    expect(res).to.eql(expected);
     done();
   });
 })
