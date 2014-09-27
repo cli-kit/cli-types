ae0547915a94f8a0d197636a5b1ea34577230037
cyberfunk <hello@cyberfunk.org>
2014-09-27 12:29:09 +0800

    Add string type test.

---
+++ test/spec/types/string.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect
+  , types = require('../../..')
+  , define = require('cli-define')
+  , Option = define.Option;
+
+describe('cli-types:', function() {
+  it('should be a string', function(done) {
+    var value = 'value';
+    var opt = new Option(
+      '-s, --string <str>', 'a string argument');
+    var res = types.string(value, opt);
+    expect(res).to.eql(value);
+    done();
+  });
+})
