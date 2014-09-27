8176e67f7fa8227c16c34863fd74ebfdb471a8b4
cyberfunk <hello@cyberfunk.org>
2014-09-27 11:57:10 +0800

    Add boolean tests.

---
+++ test/spec/types/boolean.js
@@ -0,0 +1,72 @@
+var expect = require('chai').expect
+  , types = require('../../..')
+  , define = require('cli-define')
+  , Option = define.Option;
+
+describe('cli-types:', function() {
+  it('should coerce to boolean (true)', function(done) {
+    var value = 'true';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(true);
+    done();
+  });
+  it('should coerce to boolean (false)', function(done) {
+    var value = 'false';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(false);
+    done();
+  });
+  it('should coerce to boolean (true) insensitive', function(done) {
+    var value = 'TRUE';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(true);
+    done();
+  });
+  it('should coerce to boolean (false) insensitive', function(done) {
+    var value = 'FALSE';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(false);
+    done();
+  });
+  it('should coerce to boolean (true) integer', function(done) {
+    var value = '1';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(true);
+    done();
+  });
+  it('should coerce to boolean (false) integer', function(done) {
+    var value = '0';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(false);
+    done();
+  });
+  it('should coerce to boolean (true) positive string length', function(done) {
+    var value = 'value';
+    var opt = new Option(
+      '-b, --boolean <boolean>', 'a boolean argument');
+    var res = types.boolean(value, opt);
+    expect(res).to.eql(true);
+    done();
+  });
+  //it('should coerce to array of booleans', function(done) {
+    //var cli = require('../../..')(pkg);
+    //var args = ['-b', 'true', '--boolean=False', '-b=1', '-b', '0' ];
+    //cli
+      //.option('-b, --boolean <boolean...>', 'a boolean argument', types.boolean)
+    //cli.parse(args);
+    //expect(cli.boolean).to.eql([true, false, true, false]);
+    //done();
+  //});
+})
